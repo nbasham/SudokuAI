f9f52c13f5ec0fbc4ff7e7987a43b83e60178b91
Norman Basham <2135673+nbasham@users.noreply.github.com>
2025-06-09 15:41:26 -0700

    Initial Commit

---
+++ SudokuAI/SudokuAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuAIApp.swift
+//  SudokuAI
+//
+//  Created by Norman Basham on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SudokuAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
