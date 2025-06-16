36add82c057b9ddc1e5da7a3c2ef114b12a3de76
Norman Basham <2135673+nbasham@users.noreply.github.com>
2025-06-15 19:05:42 -0700

    Added SudokuBoardView.swift

---
+++ SudokuAI/Views/SudokuBoardView.swift
@@ -0,0 +1,18 @@
+//
+//  SudokuBoardView.swift
+//  SudokuAI
+//
+//  Created by Norman Basham on 6/15/25.
+//
+
+import SwiftUI
+
+struct SudokuBoardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SudokuBoardView()
+}
